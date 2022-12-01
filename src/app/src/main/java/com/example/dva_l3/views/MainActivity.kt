3630cf87e86592e8dfcaf75f30cbb3eac0fd811c
luca zacheo <luca.zacheo1@gmail.com>
2022-12-01 12:04:36 +0100

    created base archi for mvvm app and added gradle dependencies like data binding

--- src/app/src/main/java/com/example/dva_l3/views/MainActivity.kt
+++ src/app/src/main/java/com/example/dva_l3/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dva_l3
+package com.example.dva_l3.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dva_l3.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
