4b85bd152e90c486883f453b5a0da65ac29f38e2
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-12-02 11:36:02 +0100

    pop up menu on actionbar click

--- src/app/src/main/java/com/example/dva_l3/views/MainActivity.kt
+++ src/app/src/main/java/com/example/dva_l3/views/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dva_l3.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
 import com.example.dva_l3.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.sort -> {
+                val popup = PopupMenu(this, findViewById(R.id.sort))
+                popup.inflate(R.menu.sort)
+                popup.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.sort_by_ETA -> {
+                            true
+                        }
+                        R.id.sort_by_creation_date -> {
+                            true
+                        }
+                        else -> false
+                    }
+                }
+                popup.show()
+                true
+            }
+            R.id.add -> {
+                val popup = PopupMenu(this, findViewById(R.id.add))
+                popup.inflate(R.menu.actions)
+                popup.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.generate -> {
+                            true
+                        }
+                        R.id.delete_all -> {
+                            true
+                        }
+                        else -> false
+                    }
+                }
+                popup.show()
+                true
+            }
+            else -> super.onOptionsItemSelected(menuItem)
+        }
     }
 }
